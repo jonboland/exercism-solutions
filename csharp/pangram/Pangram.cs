147585301a872d61d149db3a8d8d90faa28cdb68
jonboland <jon.boland@gmail.com>
2021-04-02 11:26:26 +0100

    Add pangram

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+
+public class Pangram
+{
+    private const string Alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    public static bool IsPangram(string input)
+    {
+        return Alphabet.All(input.ToLower().Contains);
+    }
+}
