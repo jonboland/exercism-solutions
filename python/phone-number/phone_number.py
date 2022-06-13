5be33e20725a677f7082f291309387c19945b13d
jonboland <jon.boland@gmail.com>
2022-06-13 12:24:45 +0100

    Add docstrings to phone number

--- python/phone-number/phone_number.py
+++ python/phone-number/phone_number.py
@@ -1,20 +1,22 @@
 from string import ascii_letters, punctuation
 
 
-INVALID_NUMS = {"0": "zero", "1": "one"}
-INVALID_PUNC = set(punctuation).difference("+", ".", "(", "-", ")")
+INVALID_NUMBERS = {"0": "zero", "1": "one"}
+INVALID_PUNCTUATION = set(punctuation) - set("+.()-")
+INVALID_LETTERS = set(ascii_letters)
 VALIDATION = {
     "punctuation": "punctuations not permitted",
     "letters": "letters not permitted",
     "long": "more than 11 digits",
     "short": "incorrect number of digits",
     "country": "11 digits must start with 1",
-    "area": "area code cannot start with",
-    "exchange": "exchange code cannot start with",
+    "code": "code cannot start with",
 }
 
 
 class PhoneNumber:
+    """Class to validate and format American phone numbers."""
+
     def __init__(self, phone_number):
         self.number = self._clean_number(phone_number)
         self.area_code = self.number[:3]
@@ -22,32 +24,36 @@ class PhoneNumber:
         self.subscriber = self.number[6:]
 
     def pretty(self):
+        """Return formatted phone number."""
         return f"({self.area_code})-{self.exchange_code}-{self.subscriber}"
 
     def _clean_number(self, phone_number):
-        self._validate_chars(phone_number)        
+        # Return cleaned number if it's valid
+        self._validate_chars(phone_number)
         stripped = "".join(x for x in phone_number if x.isdigit())
-        length = len(stripped)       
+        length = len(stripped)
         self._validate_length(length)
 
         if length == 11:
             self._validate_country(stripped)
             stripped = stripped[1:]
-        
+
         self._validate_codes(stripped)
 
         return stripped
 
     def _validate_chars(self, phone_number):
+        # Raise error if phone number contains letters or invalid punctuation
         unique_chars = set(phone_number)
 
-        if unique_chars.intersection(INVALID_PUNC):
+        if unique_chars & INVALID_PUNCTUATION:
             raise ValueError(VALIDATION["punctuation"])
 
-        if unique_chars.intersection(ascii_letters):
+        if unique_chars & INVALID_LETTERS:
             raise ValueError(VALIDATION["letters"])
 
     def _validate_length(self, length):
+        # Raise error if phone number's length is invalid
         if length > 11:
             raise ValueError(VALIDATION["long"])
 
@@ -55,14 +61,16 @@ class PhoneNumber:
             raise ValueError(VALIDATION["short"])
 
     def _validate_country(self, stripped):
+        # Raise error if phone number has invalid country code
         if not stripped.startswith("1"):
             raise ValueError(VALIDATION["country"])
 
     def _validate_codes(self, stripped):
+        # Raise error if area or exchange code starts with invalid digit
         first, _, _, fourth = stripped[:4]
 
-        if first in INVALID_NUMS:
-            raise ValueError(f"{VALIDATION['area']} {INVALID_NUMS[first]}")
-
-        if fourth in INVALID_NUMS:
-            raise ValueError(f"{VALIDATION['exchange']} {INVALID_NUMS[fourth]}")
+        for digit, code_type in ((first, "area"), (fourth, "exchange")):
+            if digit in INVALID_NUMBERS:
+                raise ValueError(
+                    f"{code_type} {VALIDATION['code']} {INVALID_NUMBERS[digit]}"
+                )
